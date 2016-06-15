176e928f408e1f36feea6094daf3557640af9460
fuyongde <fuyd@dachentech.com.cn>
2016-06-15 11:48:00 +0800

    hello world

---
+++ basic/exp_input.py
@@ -0,0 +1,3 @@
+print("请输入您的姓名");
+name = input();
+print("您输入的姓名为：",name);
---
+++ basic/exp_print.py
@@ -0,0 +1,18 @@
+"""
+该py文件主要用来演示python的输出函数print()
+"""
+
+#Hello World
+print("Hello World!");
+
+'''
+print()函数可以接收多个字符串，用","隔开，就可以连成一串输出
+print()会依次打印每个字符串，遇到","会输出一个空格，因此，输出的字符串是这样拼起来的
+The quick brown fox jumps over the lazy dog
+'''
+print("The quick brown fox", "jumps over", 'the lazy dog');
+
+#print()函数也可以打印整数，或者计算结果
+print(100 + 200 + 300);
+
+print("100 + 200 =", 100 + 200);
