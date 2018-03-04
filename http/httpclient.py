e24202d80c0431facbe988ed4e29299bb01e199c
fuyongde <fuyongde@foxmail.com>
2018-03-04 18:39:53 +0800

    update

--- http/httpclient.py
+++ http/httpclient.py
@@ -1,10 +1,11 @@
 import requests
-<<<<<<< HEAD
-resp = requests.get("http://localhost:8080/niki/index")
+resp = requests.get("http://localhost:8080/qin/map/test")
 print(resp.status_code)
 print(resp.text)
 print(resp.json())
-=======
-resp = requests.get("http://www.baidu.com/")
-print(resp.text)
->>>>>>> 2eeac6ae73b16449a46c64029ba49627d0c5e01f
+
+reqParams = {'username':'fuyongde', 'password':'fuyongde'}
+respParams = requests.get("http://localhost:8080/qin/map/testParams", params=reqParams)
+print(respParams.status_code)
+print(respParams.text)
+print(respParams.json())
